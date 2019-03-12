6cc7b9cc4a8d4015b58cdbed2b39e630e2f5666e
jim9112 <jim9112@gmail.com>
2019-03-11 20:50:17 -0400

    Added Selections

--- js/app.js
+++ js/app.js
@@ -6,5 +6,9 @@ let playerName = '';
 
 login.onclick = () => {
     playerName = nameInput.value;
-    modal.innerHTML = `<h1>Welcome ${playerName}`;
+    modal.innerHTML = `<h1>Welcome ${playerName}</h1><br> <form action="">
+    <input type="radio" name="gender" value="addition"> Addition<br>
+    <input type="radio" name="gender" value="subtraction"> Subtraction<br> 
+    <input type="submit" value="Submit">
+    </form>`;
 };
