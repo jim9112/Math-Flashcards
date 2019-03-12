41658dd9655221c4ed63ecab901d50b15922d901
jim9112 <jim9112@gmail.com>
2019-03-12 08:29:40 -0400

    trimmed down css

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
+// To Do
+// change classes to IDs and add a class to manage like formatting
+
+
+
+
 const nameInput = document.querySelector('#name-input');
 const login = document.querySelector('.login');
-const loginScreen = document.querySelector('.login-screen');
-const menuScreen = document.querySelector('.menu-screen');
+const loginScreen = document.querySelector('#login-screen');
+const menuScreen = document.querySelector('#menu-screen');
+const mathSelection = document.querySelector('#math-selection');
 
 let playerName = '';
 
@@ -10,4 +17,9 @@ login.onclick = () => {
     loginScreen.style.display = 'none';
     document.querySelector('#name-display').textContent = `Welcome ${playerName}!`;
     menuScreen.style.display= 'block';
+};
+
+mathSelection.onclick = (e) => {
+    e.preventDefault();
+    console.log('math selection test');
 };
