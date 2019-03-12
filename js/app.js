bab2f6728234a84b70528f23a10b7ce24b4e0750
jim9112 <jim9112@gmail.com>
2019-03-11 21:35:00 -0400

    Added Login

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 const nameInput = document.querySelector('#name-input');
 const login = document.querySelector('.login');
-const modal = document.querySelector('.modal');
+const loginScreen = document.querySelector('.login-screen');
+const menuScreen = document.querySelector('.menu-screen');
 
 let playerName = '';
 
 login.onclick = () => {
     playerName = nameInput.value;
-    modal.innerHTML = `<h1>Welcome ${playerName}</h1><br> <form action="">
-    <input type="radio" name="gender" value="addition"> Addition<br>
-    <input type="radio" name="gender" value="subtraction"> Subtraction<br> 
-    <input type="submit" value="Submit">
-    </form>`;
+    loginScreen.style.display = 'none';
+    document.querySelector('#name-display').textContent = `Welcome ${playerName}!`;
+    menuScreen.style.display= 'block';
 };
